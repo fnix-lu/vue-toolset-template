bcb4dc6e4b6b917c0e40acc6ebffb90c32979ce7
wenjun.lu <835913098@qq.com>
2021-07-16 16:54:20 +0800

    LineClamp 组件及示例

---
+++ src/components/LineClamp.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="line-clamp">
+    <input type="checkbox" id="more">
+    <div class="line-clamp-inner">
+      <label for="more" class="more"></label>
+      <span><slot></slot></span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.line-clamp {
+  margin: auto;
+  width: 500px;
+  border: 1px solid #ccc;
+  resize: horizontal;
+  margin-bottom: 24px;
+  background: #fafafa;
+  /* 以上示例容器样式 */
+
+  display: flex;
+}
+
+#more {
+  display: none;
+}
+
+.line-clamp-inner {
+  overflow: hidden;
+  line-height: 1.5;
+  text-align: justify;
+  max-height: 4.5em;
+  position: relative;
+}
+#more:checked + .line-clamp-inner {
+  max-height: none;
+}
+
+.line-clamp-inner::before {
+  content: '';
+  float: right;
+  height: 100%;
+  margin-top: -1.5em;
+}
+
+.line-clamp-inner::after {
+  content: '';
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background: #fff;
+}
+#more:checked + .line-clamp-inner::after {
+  visibility: hidden;
+}
+
+.more {
+  float: right;
+  clear: both;
+  color: #4D91F7;
+  position: relative;
+  padding-left: 1.5em;
+}
+
+.more::before {
+  content: '…';
+  position: absolute;
+  left: 0;
+  color: initial;
+}
+#more:checked + .line-clamp-inner .more::before {
+  visibility: hidden;
+}
+
+.more::after {
+  content: '展开';
+}
+#more:checked + .line-clamp-inner .more::after {
+  content: '收起';
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <line-clamp>浮动元素是如何定位的？正如我们前面提到的那样，当一个元素浮动之后，它会被移出正常的文档流，然后向左或者向右平移，一直平移直到碰到了所处的容器的边框，或者碰到另外一个浮动的元素。在下面的图片中，有三个红色的正方形。其中有两个向左浮动，一个向右浮动。要注意到第二个向左浮动的正方形被放在第一个向左浮动的正方形的右边。如果还有更多的正方形这样浮动，它们会继续向右堆放，直到填满容器一整行，之后换行至下一行。</line-clamp>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import LineClamp from '@/components/LineClamp.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    LineClamp
   }
 }
 </script>
